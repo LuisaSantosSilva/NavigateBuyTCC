d5da43cb6d111e02c1b45462fea37a58c43d2ead
Vinicius <vini.valero@gmail.com>
2024-10-01 16:50:20 -0300

    atualizando scrapy do mercado livre (faltando imagem)

--- api/mercadolivre/mercadolivre/spiders/ml.py
+++ api/mercadolivre/mercadolivre/spiders/ml.py
@@ -1,6 +1,5 @@
 import scrapy
 
-
 class MlSpider(scrapy.Spider):
     name = 'ml'
 
@@ -8,14 +7,40 @@ class MlSpider(scrapy.Spider):
 
     def parse(self, response, **kwargs):
         for i in response.xpath('//li[@class="ui-search-layout__item shops__layout-item ui-search-layout__stack"]'):
-            price = i.xpath('.//span[@class="andes-money-amount__fraction"]/text()').get()
-            cents = i.xpath('.//span[@class="andes-money-amount__cents andes-money-amount__cents--superscript-24"]/text()').get()
-            title = i.xpath('.//h2[@class="ui-search-item__title"]/text()').get()
-            link = i.xpath('./a/@href').get()
+            normal_price = i.xpath('.//span[contains(@class, "andes-money-amount--cents-superscript") and not(ancestor::s)]')
+            if normal_price:
+                price = normal_price.xpath('.//span[@class="andes-money-amount__fraction"]/text()').get(default='').strip()
+                cents = normal_price.xpath('.//span[@class="andes-money-amount__cents andes-money-amount__cents--superscript-24"]/text()').get(default='').strip()
+            else:
+                price = ''
+                cents = ''
 
-            yield {
-                'price': price,
-                'cents': cents,
-                'title': title,
-                'link': link
-            }
+            promo_price = i.xpath('.//s[@class="andes-money-amount andes-money-amount--previous"]/span[@class="andes-money-amount__fraction"]/text()').get(default='').strip()
+            promo_cents = i.xpath('.//s[@class="andes-money-amount andes-money-amount--previous"]/span[@class="andes-money-amount__cents"]/text()').get(default='').strip()
+
+            image = i.xpath('.//img[@class="ui-search-result-image__element"]/@href').get(default='').strip()
+            title = i.xpath('.//a[@class="ui-search-link__title-card ui-search-link"]/text()').get(default='').strip()
+            link = i.xpath('.//a[@class="ui-search-link__title-card ui-search-link"]/@href').get(default='').strip()
+            stars = i.xpath('.//span[@class="ui-search-reviews__rating-number"]/text()').get(default='').strip()
+            avaliations = i.xpath('.//span[@class="ui-search-reviews__amount"]/text()').get(default='').strip()
+
+            if promo_price:
+                yield {
+                    'preço promocional': promo_price,
+                    'centavos': promo_cents,
+                    'título': title,
+                    'link': link,
+                    'estrelas': stars,
+                    'avaliações': avaliations,
+                    'imagem': image
+                }
+            else:
+                yield {
+                    'preço': price,
+                    'centavos': cents,
+                    'título': title,
+                    'link': link,
+                    'estrelas': stars,
+                    'avaliações': avaliations,
+                    'imagem': image
+                }
